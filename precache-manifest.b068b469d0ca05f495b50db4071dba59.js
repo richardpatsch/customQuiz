e4ea6a4d9a7a5508c950a1fdf309279f57b1f60d
Richard <richardpatsch@users.noreply.github.com>
2019-01-30 18:44:43 +0100

    Add files via upload

---
+++ precache-manifest.b068b469d0ca05f495b50db4071dba59.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4cffa739f751f90686ec3ac6e9b56e0d",
+    "url": "/customQuiz/static/media/icon-check.4cffa739.svg"
+  },
+  {
+    "revision": "89dfd8d40b3a589b27a1",
+    "url": "/customQuiz/static/js/runtime~main.89dfd8d4.js"
+  },
+  {
+    "revision": "a4fca2f244c6ab270ac5",
+    "url": "/customQuiz/static/js/main.a4fca2f2.chunk.js"
+  },
+  {
+    "revision": "6f27e26073939a082fc9",
+    "url": "/customQuiz/static/js/1.6f27e260.chunk.js"
+  },
+  {
+    "revision": "a4fca2f244c6ab270ac5",
+    "url": "/customQuiz/static/css/main.f8344201.chunk.css"
+  },
+  {
+    "revision": "c5fc0c847ffca1cbe43c7505dc8d7706",
+    "url": "/customQuiz/index.html"
+  }
+];
